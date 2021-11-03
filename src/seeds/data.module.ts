48343c912f9a27b1f66611e668237e3fa63b7d8b
justalk <justal.kevin@gmail.com>
2021-11-03 16:58:54 +0100

    [REFACTORING] Fix linter error from faker

--- src/seeds/data.module.ts
+++ src/seeds/data.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CommandModule } from 'nestjs-command';
-import { SeedsService } from './data.service'
+import { SeedsService } from './data.service';
 import { Data, DataSchema } from './../schemas/data.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CommandModule, MongooseModule.forFeature([{ name: Data.name, schema: DataSchema }])],
+  imports: [
+    CommandModule,
+    MongooseModule.forFeature([{ name: Data.name, schema: DataSchema }]),
+  ],
   providers: [SeedsService],
-  exports: [SeedsService]
+  exports: [SeedsService],
 })
 export class SeedsModule {}
--- src/seeds/data.service.ts
+++ src/seeds/data.service.ts
@@ -1,19 +1,18 @@
-import { Command, Positional } from 'nestjs-command';
+import { Command } from 'nestjs-command';
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Data, DataDocument } from './../schemas/data.schema';
-
-const faker = require('faker');
+import * as faker from 'faker';
 
 @Injectable()
 export class SeedsService {
   constructor(@InjectModel(Data.name) private dataModel: Model<DataDocument>) {}
 
   @Command({ command: 'seeds', describe: 'Populate the database' })
-  seed(data): Promise<Data[]> {
-    this.dataModel.deleteMany().exec()
-    const datas = Array.from({ length: 5 }).map((x) => {
+  seed(): Promise<Data[]> {
+    this.dataModel.deleteMany().exec();
+    const datas = Array.from({ length: 5 }).map(() => {
       return new this.dataModel({
         name: faker.random.word(),
         date: Math.random() < 0.5 ? faker.date.future() : '2018-03-22',
